f75d75490506de3470e9731070c0c6aa878cab8f
minhthong3 <63642154+minhthong3@users.noreply.github.com>
2024-07-02 10:37:32 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,12 +5,14 @@ import requests
 import pandas as pd
 from datetime import datetime
 import time
+import json
 
 # Cấu hình Google Sheets
 GOOGLE_SHEET_URL = "https://docs.google.com/spreadsheets/d/1kkOjUihnNpcWn8jmNM7majctXlqU18fGvwlTOVi9efg/edit#gid=0"
-TELEGRAM_TOKEN = '7405333641:AAHVOn9RbL0K33_4OoZeUq0SJqS07uSlN4Q'
-TELEGRAM_CHAT_ID = '-4257628203'
-GCP_SERVICE_ACCOUNT_FILE = 'datavnwealth-25a353ea3781.json'  # Đường dẫn tới tệp JSON của bạn
+TELEGRAM_TOKEN = st.secrets["TELEGRAM_TOKEN"]
+TELEGRAM_CHAT_ID = st.secrets["TELEGRAM_CHAT_ID"]
+GCP_SERVICE_ACCOUNT_FILE = st.secrets["GCP_SERVICE_ACCOUNT_FILE"]  # JSON từ secrets
+
 
 # Hàm để lấy dữ liệu từ Google Sheets
 @st.experimental_singleton
